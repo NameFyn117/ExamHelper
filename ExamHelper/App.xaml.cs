1b9ce8ae0958bccc1aea404d3b12e41e70ec511b
NameFyn117 <a18903284212@163.com>
2026-02-01 11:16:24 +0800

    添加项目文件。

---
+++ ExamHelper/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ExamHelper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
